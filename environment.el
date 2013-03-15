7fba75fc280485ffe0b2f62494447730208d115f
Mike Gilbert <mjgilbert@gmail.com>
2013-03-14 18:55:38 -0700

    Initial edits after MVS fork.

--- environment.el
+++ environment.el
@@ -1,6 +1,8 @@
-(if (boundp 'mvs-after-loads) (funcall mvs-after-loads))
-(setq-default indent-tabs-mode nil)
+(if (boundp 'g-after-loads) (funcall g-after-loads))
+(setq-default truncate-lines t)
 (setq-default show-trailing-whitespace t)
+(setq-default indent-tabs-mode nil)
+(setq x-select-enable-clipboard t)
 (setq-default truncate-lines t)
 (setq coffee-tab-width 4)
 (setq default-tab-width 4)
@@ -8,6 +10,9 @@
 (setq rspec-shell-name "*shell*")
 (setq tab-width 4)
 (setenv "PAGER" "cat")
+(setenv "EDITOR" "emacs")
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "google-chrome")
 (column-number-mode)
 (global-linum-mode t)
 (add-hook 'shell-mode-hook
@@ -19,5 +24,5 @@
           'bash-completion-dynamic-complete)
 (add-hook 'shell-command-complete-functions
           'bash-completion-dynamic-complete)
-(if (boundp 'mvs-before-set-keyboard-mapping) (funcall mvs-before-set-keyboard-mapping))
-(set-default-keyboard-mapping "mvs")
+(if (boundp 'g-before-set-keyboard-mapping) (funcall g-before-set-keyboard-mapping))
+(set-default-keyboard-mapping "g")
--- keyboard-mappings.el
+++ keyboard-mappings.el
@@ -1,84 +1,27 @@
-(define-keyboard-mappings "mvs"
-  '(("Buffers"
-     ("C-b" switch-to-buffer)
-     ("C-w" kill-this-buffer))
+(defvar real-keyboard-keys
+  '(("M-<up>"        . "\M-[1;3A")
+    ("M-<down>"      . "\M-[1;3B")
+    ("M-<right>"     . "\M-[1;3C")
+    ("M-<left>"      . "\M-[1;3D")
+    ("C-<return>"    . "\C-j")
+    ("C-<delete>"    . "\M-[3;5~")
+    ("C-<up>"        . "\M-[1;5A")
+    ("C-<down>"      . "\M-[1;5B")
+    ("C-<right>"     . "\M-[1;5C")
+    ("C-<left>"      . "\M-[1;5D"))
+  "An assoc list of pretty key strings
+and their terminal equivalents.")
 
-    ("Windows"
-     ("C-<tab>" other-window)
-     ("<escape> <tab>" other-window)
-     ("M-<tab>" other-window)
-     ("M-TAB" other-window)
-     ("<backtab>" i-previous-window)
-     ("M-1" delete-other-windows)
-     ("<escape> 1" delete-other-windows)
-     ("M-2" split-window-vertically)
-     ("<escape> 2" split-window-vertically)
-     ("M-3" split-window-horizontally)
-     ("<escape> 3" split-window-horizontally)
-     ("M-0" delete-window)
-     ("<escape> 0" delete-window))
+(defun key (desc)
+  (or (and window-system (read-kbd-macro desc))
+      (or (cdr (assoc desc real-keyboard-keys))
+          (read-kbd-macro desc))))
 
-    ("Copy/Paste"
-     ("C-c" kill-ring-save)
-     ("C-x" kill-region)
-     ("C-v" yank)
-     ("M-v" yank-pop)
-     ("C-z" undo))
-
-    ("Find"
-     ("C-f" isearch-forward)
-     ("M-f" isearch-backward))
-
-    ("Rectangle"
-     ("C-r r" string-rectangle)
-     ("C-r x" kill-rectangle)
-     ("C-r v" yank-rectangle))
-
-    ("Save/Load"
-     ("C-o" find-file)
-     ("C-s" save-buffer)
-     ("M-s" write-file))
-
-    ("Jump"
-     ("C-j c" jump-to-rails-controller)
-     ("C-j h" jump-to-rails-helper)
-     ("C-j m" jump-to-rails-model)
-     ("C-j s" jump-to-rails-spec)
-     ("C-j t" jump-to-rails-test)
-     ("C-j v" jump-to-rails-view))
-
-    ("Lisp Eval"
-     ("M-e" eval-last-sexp))
-
-    ("Exit"
-     ("C-q" save-buffers-kill-terminal))
-
-    ("Suspend"
-     ("M-z" suspend-frame))
-
-    ("Movement"
-     ("C-<down>" scroll-up)
-     ("C-<up>" scroll-down))
-
-    ("Interrupt"
-     ("s-c" comint-interrupt-subjob)
-     ("<f9> c" comint-interrupt-subjob)))
-
-  ;; On load
-  (lambda ()
-    (defun i-previous-window ()
-      "Interactive version of previous-window"
-      (interactive)
-      (select-window (previous-window (selected-window))))
-    (setq old-isearch-mode-map-hashmap (make-hash-table :test 'equal))
-    (puthash "C-f" (lookup-key isearch-mode-map (kbd "C-f")) old-isearch-mode-map-hashmap)
-    (puthash "M-f" (lookup-key isearch-mode-map (kbd "M-f")) old-isearch-mode-map-hashmap)
-    (define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
-    (define-key isearch-mode-map (kbd "M-f") 'isearch-repeat-backward))
-
-  ;; On unload
-  (lambda ()
-    (define-key isearch-mode-map (kbd "C-f") (gethash "C-f" old-isearch-mode-map-hashmap))
-    (define-key isearch-mode-map (kbd "M-f") (gethash "M-f" old-isearch-mode-map-hashmap))
-    (define-key keyboard-mappings-keymap (kbd "C-r") nil)
-    (define-key keyboard-mappings-keymap (kbd "C-j") nil)))
+(define-keyboard-mappings "g"
+  '(("Windows"
+      ("M-<left>" windmove-left)
+      ("M-<right>" windmove-right)
+      ("M-<up>" windmove-up)
+      ("M-<down>" windmove-down))
+    ("Editing"
+      ("C-z" undo))))
