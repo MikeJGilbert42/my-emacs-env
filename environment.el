ce58a81a68c5b049513962584483e813e28c8f5d
Mike Gilbert <mjgilbert@gmail.com>
2013-08-13 15:24:55 -0700

    Enable x clipboard settings and turn on column number mode instead of toggling.

--- environment.el
+++ environment.el
@@ -6,14 +6,16 @@
 (setq-default truncate-lines t)
 (setq coffee-tab-width 4)
 (setq default-tab-width 4)
+(setq-default fill-column 72)
 (setq inhibit-splash-screen t)
 (setq rspec-shell-name "*shell*")
 (setq tab-width 4)
+(setq x-select-enable-clipboard t)
 (setenv "PAGER" "cat")
 (setenv "EDITOR" "emacs")
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
-(column-number-mode)
+(column-number-mode t)
 (delete-selection-mode)
 (global-linum-mode t)
 (add-hook 'shell-mode-hook
